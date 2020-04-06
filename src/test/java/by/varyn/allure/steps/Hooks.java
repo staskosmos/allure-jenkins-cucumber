255a69c2e51bd0e040aa7ec5e3cab1076fc629f1
staskosmos <mr.varyn@mail.ru>
2020-04-07 00:29:19 +0300

    add Hooks

---
+++ src/test/java/by/varyn/allure/steps/Hooks.java
@@ -0,0 +1,12 @@
+package by.varyn.allure.steps;
+
+import io.cucumber.java.Before;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Hooks {
+    @Before
+    public void openUrl() {
+        open("https://grinfer.com/");
+    }
+}
